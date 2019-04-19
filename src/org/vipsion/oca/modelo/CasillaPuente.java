073174d19b51c60d8a35ca883e4022d6d52b7bf4
AlvaroDAM98 <49791545+AlvaroDAM98@users.noreply.github.com>
2019-04-19 20:03:35 +0200

    Add files via upload

---
+++ src/org/vipsion/oca/modelo/CasillaPuente.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.vipsion.oca.modelo;
+
+/**
+ *
+ * @author Alvaro
+ */
+public class CasillaPuente extends Casilla{
+
+    public CasillaPuente(int posicion) {
+        super(posicion);
+    }
+    
+}
